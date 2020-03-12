88a3627bc7cbb61554a64a71ba6ddf4683f8a1cc
Rigorich <61323493+Rigorich@users.noreply.github.com>
2020-03-12 23:14:05 +0300

    Add files via upload

--- deRigorich.c
+++ deRigorich.c
@@ -1,98 +1,228 @@
 #include "deRigorich.h"
 
-/*  */
+/* */
 
 void TestFunction(void) {
-	printf("Hello, World!\n");
+    printf("Hello, World!\n");
 }
+
+void FatalError(char* message) { 
+    printf("%s", message); 
+    sleep(5); 
+    getch(); 
+    exit(42); 
+};
 
 void MallocCheck(void* ptr) {
     if (!ptr) {
-        printf("Not enough heap memory!");
-        ERROR
+        FatalError("Not enough heap memory!");
     }
+}
+
+void* Malloc(long long ByteCount) {
+    if (ByteCount < 0) {
+        printf("Warning: try to alloc wrong byte amount!\n");
+        return NULL;
+    }
+    void* ptr = malloc(ByteCount);
+    while (!ptr) {
+        printf("\n");
+        printf("Not enough heap memory!\n");
+        printf("Please close Google Chrome or this program!\n");
+        printf("And press any key\n");
+        getch();
+        ptr = malloc(ByteCount);
+    }
+    return ptr;
+}
+
+void swap(void* a, void* b, int size) {
+    void* tmp = Malloc(size);
+    memcpy(tmp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, tmp, size);
+    free(tmp);
 }
 
 /* */
 
+/* Comparators */ 
+
+bool CmpInt(void* a, void* b) {
+    return TO(int)a < TO(int)b;
+}
+
+bool CmpDouble(void* a, void* b) {
+    return TO(double)a < TO(double)b;
+}
+
+bool CmpChar(void* a, void* b) {
+    return TO(char)a < TO(char)b;
+}
+
+bool CmpCharArray(void* a, void* b) {
+    if(strcmp(TO(char*)a, TO(char*)b) < 0) return true;
+    return false;
+}
+
+bool OperLess(void* a, void* b, bool (*cmp)(void*, void*)) {
+    return cmp(a, b);
+}
+
+bool OperEqual(void* a, void* b, bool (*cmp)(void*, void*)) {
+    return (!cmp(a, b) && !cmp(b, a));
+}
+
+bool OperGreat(void* a, void* b, bool (*cmp)(void*, void*)) {
+    return cmp(b, a);
+}
+
+/* Comparators */
 
 /* Vector */
 
 Vector* VectorNew(int sizeof_element) {
-	Vector* t = malloc(sizeof(Vector));
-	MallocCheck(t);
-	t->elemsize = sizeof_element;
-	t->size = 0;
-	t->begin = NULL;
-	t->alloc = 0;
-	return t;
+    Vector* t = Malloc(sizeof(Vector));
+    t->elemsize = sizeof_element;
+    t->size = 0;
+    t->begin = NULL;
+    t->alloc = 0;
+    t->comp = NULL;
+    return t;
 }
 
 void VectorClear(Vector* this) { 
-	free(this->begin);
-	this->begin = NULL;
-	this->size = 0;
-	this->alloc = 0;
+    free(this->begin);
+    this->begin = NULL;
+    this->size = 0;
+    this->alloc = 0;
+    this->comp = NULL;
 }
 
 void VectorDelete(Vector* this) { 
-	VectorClear(this);
-	free(this);
-	this = NULL;
+    VectorClear(this);
+    free(this);
+    /* this = NULL; */
 }
 
 void VectorAddElem(Vector* this, void* what) {
     if (this->size + 1 > this->alloc) {
-		VectorRealloc(this, this->alloc * 2 + 1);
+        VectorRealloc(this, this->alloc * 2 + 1);
     }
-	this->size += 1;
-	VectorSetElem(this, this->size - 1, what);
+    this->size += 1;
+    VectorSetElem(this, this->size - 1, what);
+}
+
+void VectorDelElem(Vector* this, int index) {
+    if (index < 0 || VectorSize(this) <= index) {
+        printf("Error: Wrond index (in VectorDelElem)");
+        return;
+    }
+    int i;
+    for (i = index + 1; i < VectorSize(this); i++) {
+        VectorSetElem(this, i-1, VectorGetElem(this, i));
+    }
+    this->size -= 1;
 }
 
 int VectorSize(Vector* this) { 
-	return this->size; 
+    return this->size; 
+}
+
+void* VectorBegin(Vector* this) {
+    return this->begin;
 }
 
 void* VectorGetElem(Vector* this, int index) { 
-	if (index < 0 || VectorSize(this) <= index) {
-		printf("Error: Wrond index (in Vector)");
-		return;
-	}
-	return (void*)(this->begin + index * this->elemsize);
+    if (index < 0 || VectorSize(this) <= index) {
+        printf("Error: Wrond index (in VectorGetElem)");
+        return;
+    }
+    return (void*)(this->begin + index * this->elemsize);
 }
 
 void VectorSetElem(Vector* this, int index, void* what) { 
-	if (index < 0 || VectorSize(this) <= index) {
-		printf("Error: Wrond index (in Vector)");
-		return;
-	}
-	memcpy((this->begin + index * this->elemsize), what, this->elemsize);
+    if (index < 0 || VectorSize(this) <= index) {
+        printf("Error: Wrond index (in VectorSetElem)");
+        return;
+    }
+    memcpy((this->begin + index * this->elemsize), what, this->elemsize);
+}
+
+void* VectorGetComp(Vector* this) {
+    if (!this->comp) {
+        FatalError("Error: Vector has no Comparator");
+    }
+    return this->comp;
+}
+
+void VectorSetComp(Vector* this, bool (*comparator) (void* a, void* b)) {
+    this->comp = comparator;
+}
+
+int VectorFindElem(Vector* this, void* what) {
+    int i;
+    for (i = 0; i < VectorSize(this); i++) {
+        if (OperEqual(VectorGetElem(this, i), what, VectorGetComp(this))) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int VectorFindLastElem(Vector* this, void* what) {
+    int i;
+    for (i = VectorSize(this)-1; i >= 0; i--) {
+        if (OperEqual(VectorGetElem(this, i), what, VectorGetComp(this))) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool VectorContainElem(Vector* this, void* what) {
+    return VectorFindElem(this, what) != -1;
+}
+
+void VectorSort(Vector* this){
+    int i, j;
+    for(i = 0; i < VectorSize(this); i++)
+    for(j = i + 1; j < VectorSize(this); j++)
+    if (OperLess(VectorGetElem(this, j), VectorGetElem(this, i), VectorGetComp(this))) {
+        swap(VectorGetElem(this, j), VectorGetElem(this, i), this->elemsize);
+    }
+}
+
+void VectorSortComp(Vector* this, bool (*comp) (void* a, void* b)){
+    void* tmp = this->comp;
+    VectorSetComp(this, comp);
+    VectorSort(this);
+    VectorSetComp(this, tmp);
 }
 
 void VectorRealloc(Vector* this, int newalloc) {
-	if (newalloc < this->size) {
-		printf("Warning: Wrong Vector Realloc!\n");
-		return;
-	}
+    if (newalloc < this->size) {
+        printf("Warning: Wrong Vector Realloc!\n");
+        return;
+    }
     this->alloc = newalloc;
-	if (newalloc == 0) {
-		free(this->begin);
+    if (newalloc == 0) {
+        free(this->begin);
         this->begin = NULL;
-		return;
-	}
-	void* t = malloc(newalloc * this->elemsize);
-	MallocCheck(t);
-	memcpy(t, this->begin, this->size * this->elemsize);
-	free(this->begin);
-	this->begin = t;
+        return;
+    }
+    void* t = Malloc(newalloc * this->elemsize);
+    memcpy(t, this->begin, this->size * this->elemsize);
+    free(this->begin);
+    this->begin = t;
 }
 
 void VectorResize(Vector* this, int newsize) {
-	if (newsize < 0) {
-		printf("Warning: New Vector size < 0\n");
-		newsize = 0;
-	}
-	this->size = newsize;
+    if (newsize < 0) {
+        printf("Warning: New Vector size < 0\n");
+        newsize = 0;
+    }
+    this->size = newsize;
     VectorRealloc(this, newsize);
 }
 
@@ -104,71 +234,97 @@ void VectorResize(Vector* this, int newsize) {
 char NullChar = 0;
 
 Vector* StringNew(void) {
-	Vector* t = VectorNew(sizeof(char));
-	VectorAddElem(t, &NullChar);
-	return t;
+    Vector* t = VectorNew(sizeof(char));
+    VectorSetComp(t, CmpChar);
+    VectorAddElem(t, &NullChar);
+    return t;
 }
 
 void StringClear(Vector* this) { 
-	VectorClear(this);
-	VectorAddElem(this, &NullChar);
+    VectorClear(this);
+    VectorAddElem(this, &NullChar);
 }
 
 void StringDelete(Vector* this) { 
-	VectorDelete(this);
+    VectorDelete(this);
 }
 
 void StringAddElem(Vector* this, char what) { 
-	VectorAddElem(this, &NullChar);
-	VectorSetElem(this, StringSize(this) - 1, &what);
+    VectorAddElem(this, &NullChar);
+    VectorSetElem(this, StringSize(this) - 1, &what);
+}
+
+void StringDelElem(Vector* this, int index) { 
+    VectorDelElem(this, index);
 }
 
 char* StringBegin(Vector* this) {
-	return this->begin;
+    return this->begin;
 }
 
 int StringSize(Vector* this) { 
-	return this->size - 1; 
+    return this->size - 1; 
 }
 
 char StringGetElem(Vector* this, int index) { 
-	return TO(char)VectorGetElem(this, index); 
+    return TO(char)VectorGetElem(this, index); 
 }
 
 void StringSetElem(Vector* this, int index, char what) { 
-	VectorSetElem(this, index, &what); 
+    VectorSetElem(this, index, &what); 
+}
+
+int StringFindElem(Vector* this, char what) {
+    return VectorFindElem(this, &what);
+}
+
+int StringFindLastElem(Vector* this, char what) {
+    return VectorFindLastElem(this, &what);
+}
+
+bool StringContainElem(Vector* this, char what) {
+    if (!what) {
+        return false;
+    }
+    return VectorContainElem(this, &what);
 }
 
 void StringIgnore(FILE* from) {
-	char sym;
-	fscanf(from, "%c", &sym);
-	while (sym != '\n') {
-		fscanf(from, "%c", &sym);
+    char sym;
+    fscanf(from, "%c", &sym);
+    while (sym != '\n') {
+        fscanf(from, "%c", &sym);
     }
 }
 
 void StringRead(Vector* this, char until, FILE* from) {
-	StringClear(this);
-	char sym;
-	fscanf(from, "%c", &sym);
-	while (sym == ' ' || sym == '\n' || sym == until) { 
-		fscanf(from, "%c", &sym);
+    StringClear(this);
+    char sym;
+    if (!feof(from)) {
+        fscanf(from, "%c", &sym);
     }
-	while (sym != until && sym != '\n') {
-		StringAddElem(this, sym);
-		fscanf(from, "%c", &sym);
-	}
-	ungetc(sym, from);
+    while ((sym == ' ' || sym == '\n' || sym == until) && !feof(from)) { 
+        fscanf(from, "%c", &sym);
+    }
+    while (sym != until && sym != '\n' && !feof(from)) {
+        StringAddElem(this, sym);
+        fscanf(from, "%c", &sym);
+    }
+    if (!feof(from)) {
+        ungetc(sym, from);
+    }
 }
 
 void StringReadLn(Vector* this, FILE* from) {
-	StringClear(this);
-	char sym;
-	fscanf(from, "%c", &sym);
-	while (sym != '\n') {
-		StringAddElem(this, sym);
-		fscanf(from, "%c", &sym);
-	}
+    StringClear(this);
+    char sym;
+    if (!feof(from)) {
+        fscanf(from, "%c", &sym);
+    }
+    while (sym != '\n' && !feof(from)) {
+        StringAddElem(this, sym);
+        fscanf(from, "%c", &sym);
+    }
 }
 
 bool StringToInt(char* from, long long* towhat, long long MinBound, long long MaxBound) {
@@ -176,13 +332,16 @@ bool StringToInt(char* from, long long* towhat, long long MinBound, long long Ma
         printf("Wrong StringToInt bounds!\n");
         return false;
     }
-	bool Minus = false;
-	if (from[0] == '-') {
-		Minus = true;
+    if (!from[0]) {
+        return false;
     }
-	unsigned long long t = 0;
-	int i;
-	for (i = Minus; from[i]; i++) {
+    bool Minus = false;
+    if (from[0] == '-') {
+        Minus = true;
+    }
+    unsigned long long t = 0;
+    int i;
+    for (i = Minus; from[i]; i++) {
         if (from[i] < '0' || '9' < from[i]) {
             return false;
         }
@@ -198,28 +357,32 @@ bool StringToInt(char* from, long long* towhat, long long MinBound, long long Ma
         return false;
     }
     long long tmp = t;
-	if (Minus) {
+    if (Minus) {
         tmp = -tmp;
     }
-	if (tmp < MinBound || MaxBound < tmp) {
+    if (tmp < MinBound || MaxBound < tmp) {
         return false;
     }
-	*towhat = tmp;
-	return true;
+    *towhat = tmp;
+    return true;
 }
 
 bool StringToFloat(char* from, double* towhat) {
-	bool Minus = false;
-	if (from[0] == '-') {
-		Minus = true;
+    if (!from[0]) {
+        return false;
     }
-	double t = 0.0;
-	int i;
-	for (i = Minus; from[i]; i++){
+    bool Minus = false;
+    if (from[0] == '-') {
+        Minus = true;
+    }
+    double t = 0.0;
+    int i = Minus;
+    for ( ; from[i]; i++){
         if (from[i] == '.' || from[i] == ',') {
             if (i == Minus) {
                 return false;
             }
+            i++;
             break;
         }
         else if (from[i] < '0' || '9' < from[i]) {
@@ -230,11 +393,8 @@ bool StringToFloat(char* from, double* towhat) {
             t += (from[i] - '0');
         }
     }
-	if (from[i] == '.' || from[i] == ',') {
-        i++;
-    }
-	double p = 1.0;
-	for ( ; from[i]; i++) {
+    double p = 1.0;
+    for ( ; from[i]; i++) {
         if (from[i] < '0' || '9' < from[i]) {
             return false;
         }
@@ -243,35 +403,39 @@ bool StringToFloat(char* from, double* towhat) {
             t += p * (from[i] - '0');
         }
     }
-	if (Minus) {
+    if (Minus) {
         t = -t;
     }
-	*towhat = t;
-	return true;
+    *towhat = t;
+    return true;
 }
 
 long long StringReadInt(FILE* from, long long MinBound, long long MaxBound) {
-	Vector* s = StringNew();
-	StringRead(s, ' ', from);
-	long long t;
-	while (!StringToInt(s->begin, &t, MinBound, MaxBound)) {
-		printf("Wrong integer \"%s\".\n Try again:\n", s->begin);
-		StringRead(s, ' ', from);
-	}
-	VectorDelete(s);
-	return t;
+    Vector* s = StringNew();
+    StringRead(s, ' ', from);
+    long long t;
+    while (!StringToInt(s->begin, &t, MinBound, MaxBound)) {
+        printf("Wrong integer \"%s\".\n Try again:\n", s->begin);
+        if (feof(from)) {
+            printf("Not enough data in file!\n");
+            sleep(5); getch(); exit(42);
+        }
+        StringRead(s, ' ', from);
+    }
+    VectorDelete(s);
+    return t;
 }
 
 double StringReadFloat(FILE* from) {
-	Vector* s = StringNew();
-	StringRead(s, ' ', from);
-	double t;
-	while (!StringToFloat(s->begin, &t)) {
-		printf("Wrong float \"%s\".\n Try again:\n", s->begin);
-		StringRead(s, ' ', from);
-	}
-	VectorDelete(s);
-	return t;
+    Vector* s = StringNew();
+    StringRead(s, ' ', from);
+    double t;
+    while (!StringToFloat(s->begin, &t)) {
+        printf("Wrong float \"%s\".\n Try again:\n", s->begin);
+        StringRead(s, ' ', from);
+    }
+    VectorDelete(s);
+    return t;
 }
 
 /* String */
@@ -280,33 +444,133 @@ double StringReadFloat(FILE* from) {
 /* SomeMath */
 
 double absf(double x) {
-	if (x < 0.0) return -x;
-	return x;
+    if (x < 0.0) return -x;
+    return x;
 }
 
 long long fact_int(int x) {
-	long long t = 1;
+    long long t = 1;
     if (x < 0 || 20 < x) {
         return 0;
     }
-	for ( ; x > 0; x--) t *= x;
-	return t;
+    for ( ; x > 0; x--) t *= x;
+    return t;
 }
 
 double fact_float(int x) {
-	double t = 1.0;
-	for ( ; x > 0; x--) t *= x;
+    double t = 1.0;
+    for ( ; x > 0; x--) t *= x;
     if (!finite(t) || x < 0) {
         return 0;
     }
-	return t;
+    return t;
 }
 
 /* SomeMath */
 
 
-/* */
+/* Set */
+
+Set* SetNew(int sizeof_element, bool (*comparator) (void* a, void* b)){
+    Set* t = Malloc(sizeof(Set));
+    t->elemsize = sizeof_element;
+    t->size = 0;
+    t->base = VectorNew(sizeof_element);
+    VectorSetComp(t->base, comparator);
+    t->comp = comparator;
+    return t;
+}
+
+void SetClear(Set* this) { 
+    VectorClear(this->base);
+    VectorSetComp(this->base, this->comp);
+    this->size = 0;
+}
+
+void SetDelete(Set* this) { 
+    SetClear(this);
+    free(this);
+    /* this = NULL; */
+}
+
+void SetAddElem(Set* this, void* what) {
+    if (!VectorContainElem(this->base, what)) {
+        VectorAddElem(this->base, what);
+        this->size += 1;
+        VectorSort(this->base);
+    }
+}
+
+void SetDelElem(Set* this, void* what) {
+    if (VectorContainElem(this->base, what)) {
+        VectorDelElem(this->base, VectorFindElem(this->base, what));
+        this->size -= 1;
+    }
+}
+
+bool SetContainElem(Set* this, void* what) {
+    return VectorContainElem(this->base, what);
+}
+
+int SetSize(Set* this) { 
+    return this->size; 
+}
+
+/* Set */
 
 
+/* Map */
 
-/* */
+Map* MapNew(int sizeof_key, int sizeof_value, bool (*comparator) (void* a, void* b), void (*value_nullator) (void* a)){
+    Map* t = Malloc(sizeof(Map));
+    t->keysize = sizeof_key;
+    t->valuesize = sizeof_value;
+    t->size = 0;
+    t->keys = VectorNew(sizeof_key);
+    VectorSetComp(t->keys, comparator);
+    t->values = VectorNew(sizeof_value);
+    t->comp = comparator;
+    t->nuller = value_nullator;
+    return t;
+}
+
+void MapClear(Map* this) { 
+    VectorClear(this->keys);
+    VectorSetComp(this->keys, this->comp);
+    VectorClear(this->values);
+    this->size = 0;
+}
+
+void MapDelete(Map* this) { 
+    MapClear(this);
+    free(this);
+    /* this = NULL; */
+}
+
+void* MapValue(Map* this, void* what) {
+    if (VectorContainElem(this->keys, what)) {
+        return VectorGetElem(this->values, VectorFindElem(this->keys, what));
+    } else {
+        this->size += 1;
+        VectorAddElem(this->keys, what);
+        void* tmp = Malloc(this->valuesize);
+        VectorAddElem(this->values, tmp);
+        free(tmp);
+        this->nuller(VectorGetElem(this->values, VectorSize(this->values)-1));
+        return VectorGetElem(this->values, VectorFindElem(this->keys, what));
+    }
+}
+
+bool MapContainElem(Map* this, void* what) {
+    return VectorContainElem(this->keys, what);
+}
+
+int MapSize(Map* this) { 
+    return this->size; 
+}
+
+void NullInt(void* a) {
+    (TO(int) a) = 0;
+}
+
+/* Map */
--- deRigorich.h
+++ deRigorich.h
@@ -1,6 +1,14 @@
 
-/* Version 21.02.2020 */
-/* РђРІС‚РѕСЂ: РќРёРєРёС‚Р° Р“СЂРёРіРѕСЂСЊРµРІРёС‡ (Р“СЂРёС€Р°РµРІ), СЃС‚.РіСЂ. 953501 */
+/* Version 12.03.2020 BETA */
+/* Автор: Никита Григорьевич (Гришаев), ст.гр. 953501 */
+
+/*
+    Русский язык при вводе и выводе:
+    - Кодировка файла -- ANSI
+    - #include <windows.h>
+    - SetConsoleCP(1251); 
+    - SetConsoleOutputCP(1251);
+*/
 
 #ifndef DERIGORICH_H
 #define DERIGORICH_H
@@ -9,84 +17,150 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* РџСЂРµРѕР±СЂР°Р·РѕРІС‹РІР°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ Рє С‚РёРїСѓ // TO(int)(void* p) == *(int*)(void* p) */
+/* Преобразовывает указатель к типу // TO(int)(void* p) == *(int*)(void* p) */
 #define TO(T) *(T*)
 
-/* РўРёРї РґР°РЅРЅС‹С… bool */
+/* Тип данных bool */
 #define bool int
 #define true 1
 #define false 0
 
-/* Р“СЂР°РЅРёС†С‹ С†РµР»РѕС‡РёСЃР»РµРЅРЅС‹С… С‚РёРїРѕРІ */
+/* Границы целочисленных типов */
 #define MAXINT 1000000000
 #define MAXLL 1000000000000000000
 #define INTBOUNDS -MAXINT, MAXINT
 #define LLBOUNDS -MAXLL, MAXLL
 
-/* Р¤СѓРЅРєС†РёСЏ СЌРєСЃС‚СЂРµРЅРЅРѕРіРѕ Р·Р°РІРµСЂС€РµРЅРёСЏ РїСЂРѕРіСЂР°РјРјС‹ */
-#define ERROR sleep(5); exit(42);
-
-/* Р¤СѓРЅРєС†РёРё РјРёРЅРёРјСѓРјР° Рё РјР°РєСЃРёРјСѓРјР° */
+/* Функции минимума и максимума */
 #define min(x,y) (((x) < (y)) ? (x) : (y))
 #define max(x,y) (((x) > (y)) ? (x) : (y))
 
-/* РљРѕРґС‹ РЅРµРєРѕС‚РѕСЂС‹С… СЃРїРµС†РёР°Р»СЊРЅС‹С… РєР»Р°РІРёС€ */
+/* Коды некоторых специальных клавиш */
 #define ESC 27
 #define SPACE 32
 #define ENTER 13
 #define TAB 9
 
-/* РўРµСЃС‚-С„СѓРЅРєС†РёСЏ */
+/* Тест-функция */
 void TestFunction(void); 
 
-/* РџСЂРѕРІРµСЂРєР° РЅР° СѓСЃРїРµС€РЅРѕРµ РІС‹РґРµР»РµРЅРёРµ РїР°РјСЏС‚Рё */
-void MallocCheck(void* ptr);
+/* Функция экстренного завершения программы */
+void FatalError(char* message);
 
-/* Р’РµРєС‚РѕСЂ */
+/* Выделение памяти */
+void* Malloc(long long ByteCount); /* Выделение памяти с проверкой */
+void MallocCheck(void* ptr); /* Проверка на успешное выделение памяти */
+
+/* Полезности */
+void swap(void* a, void* b, int size); /* Обмен местами двух элементов размера size */
+
+/* Вектор */
 typedef struct {
-	int elemsize;
-	int size;
-	void* begin;
-	int alloc;
+    int elemsize;
+    int size;
+    void* begin;
+    int alloc;
+    bool (*comp) (void* a, void* b);
 } Vector;
 
-Vector* VectorNew(int sizeof_element); /* РљРѕРЅСЃС‚СЂСѓРёСЂСѓРµС‚ РІРµРєС‚РѕСЂ Рё РІРѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РЅРµРіРѕ */
-void VectorDelete(Vector* this); /* Р”РµСЃС‚СЂСѓРєС‚РѕСЂ РІРµРєС‚РѕСЂР° */
-void VectorClear(Vector* this); /* РћС‡РёС‰Р°РµС‚ РІРµРєС‚РѕСЂ */
-void VectorAddElem(Vector* this, void* what); /* Р”РѕР±Р°РІР»СЏРµС‚ РІ РєРѕРЅРµС† РІРµРєС‚РѕСЂР° СЌР»РµРјРµРЅС‚ РїСѓС‚С‘Рј РєРѕРїРёСЂРѕРІР°РЅРёСЏ */
-int VectorSize(Vector* this); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ РєРѕР»РёС‡РµСЃС‚РІРѕ СЌР»РµРјРµРЅС‚РѕРІ РІРµРєС‚РѕСЂР° */
-void* VectorGetElem(Vector* this, int index); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° СЌР»РµРјРµРЅС‚ СЃ СѓРєР°Р·Р°РЅРЅС‹Рј РёРЅРґРµРєСЃРѕРј // РСЃРїРѕР»СЊР·СѓР№С‚Рµ TO(type) */
-void VectorSetElem(Vector* this, int index, void* what); /* РР·РјРµРЅСЏРµС‚ СЌР»РµРјРµРЅС‚ СЃ СѓРєР°Р·Р°РЅРЅС‹Рј РёРЅРґРµРєСЃРѕРј РїСѓС‚С‘Рј РєРѕРїРёСЂРѕРІР°РЅРёСЏ */
-void VectorResize(Vector* this, int newsize); /* РР·РјРµРЅСЏРµС‚ РєРѕР»РёС‡РµСЃС‚РІРѕ СЌР»РµРјРµРЅС‚РѕРІ РјР°СЃСЃРёРІР° */
-void VectorRealloc(Vector* this, int newalloc); /* РР·РјРµРЅСЏРµС‚ РєРѕР»РёС‡РµСЃС‚РІРѕ РїР°РјСЏС‚Рё, РІС‹РґРµР»РµРЅРЅРѕР№ РґР»СЏ РјР°СЃСЃРёРІР° */
-/* Р’РµРєС‚РѕСЂ */
+Vector* VectorNew(int sizeof_element); /* Конструирует вектор и возвращает указатель на него */
+void VectorDelete(Vector* this); /* Деструктор вектора */
+void VectorClear(Vector* this); /* Очищает вектор */
+void VectorAddElem(Vector* this, void* what); /* Добавляет в конец вектора элемент путём копирования */
+void VectorDelElem(Vector* this, int index); /* Удаляет элемент вектора с указанным индексом */
+int VectorSize(Vector* this); /* Возвращает количество элементов вектора */
+void* VectorBegin(Vector* this); /* Возвращает указатель на начало массива */
+void* VectorGetElem(Vector* this, int index); /* Возвращает указатель на элемент с указанным индексом // Используйте TO(type) */
+void VectorSetElem(Vector* this, int index, void* what); /* Изменяет элемент с указанным индексом путём копирования */
+int VectorFindElem(Vector* this, void* what); /* Возвращает индекс первого вхождения элемента или -1 */
+int VectorFindLastElem(Vector* this, void* what); /* Возвращает индекс последнего вхождения элемента или -1 */
+bool VectorContainElem(Vector* this, void* what); /* Возвращает существование вхождения элемента */
+void VectorSort(Vector* this); /* Сортирует вектор */
+void VectorSortComp(Vector* this, bool (*comp) (void* a, void* b)); /* Сортирует вектор с использованием компаратора */
+void VectorResize(Vector* this, int newsize); /* Изменяет количество элементов массива */
+void VectorRealloc(Vector* this, int newalloc); /* Изменяет количество памяти, выделенной для массива */
+/* Вектор */
 
-/* РЎС‚СЂРѕРєР° */
-Vector* StringNew(void); /* РљРѕРЅСЃС‚СЂСѓРёСЂСѓРµС‚ СЃС‚СЂРѕРєСѓ Рё РІРѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РЅРµС‘ */
-void StringDelete(Vector* this); /* Р”РµСЃС‚СЂСѓРєС‚РѕСЂ СЃС‚СЂРѕРєРё */
-void StringClear(Vector* this); /* РћС‡РёС‰Р°РµС‚ СЃС‚СЂРѕРєСѓ */
-void StringAddElem(Vector* this, char what); /* Р”РѕР±Р°РІР»СЏРµС‚ РІ РєРѕРЅРµС† СЃС‚СЂРѕРєРё СЃРёРјРІРѕР» */
-char* StringBegin(Vector* this); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РЅР°С‡Р°Р»Рѕ СЃС‚СЂРѕРєРё */
-int StringSize(Vector* this); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ РґР»РёРЅСѓ СЃС‚СЂРѕРєРё */
-char StringGetElem(Vector* this, int index); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ СЃРёРјРІРѕР» СЃС‚СЂРѕРєРё СЃ СѓРєР°Р·Р°РЅРЅС‹Рј РёРЅРґРµРєСЃРѕРј */
-void StringSetElem(Vector* this, int index, char what); /* РР·РјРµРЅСЏРµС‚ СЃРёРјРІРѕР» СЃС‚СЂРѕРєРё СЃ СѓРєР°Р·Р°РЅРЅС‹Рј РёРЅРґРµРєСЃРѕРј */
+/* Строка */
+Vector* StringNew(void); /* Конструирует строку и возвращает указатель на неё */
+void StringDelete(Vector* this); /* Деструктор строки */
+void StringClear(Vector* this); /* Очищает строку */
+void StringAddElem(Vector* this, char what); /* Добавляет в конец строки символ */
+/*UPD*/void StringPutElem(Vector* this, int index, char what); /* Добавляет символ в место с указанным индексом */
+void StringDelElem(Vector* this, int index); /* Удаляет символ с указанным индексом */
+char* StringBegin(Vector* this); /* Возвращает указатель на начало строки */
+int StringSize(Vector* this); /* Возвращает длину строки */
+char StringGetElem(Vector* this, int index); /* Возвращает символ строки с указанным индексом */
+void StringSetElem(Vector* this, int index, char what); /* Изменяет символ строки с указанным индексом */
+int StringFindElem(Vector* this, char what); /* Возвращает индекс первого вхождения символа или -1 */
+int StringFindLastElem(Vector* this, char what); /* Возвращает индекс последнего вхождения символа или -1 */
+bool StringContainElem(Vector* this, char what); /* Возвращает существование вхождения символа */
 
-void StringRead(Vector* this, char until, FILE* from); /* Р§РёС‚Р°РµС‚ СЃС‚СЂРѕРєСѓ РјРµР¶РґСѓ РґРІСѓРјСЏ СЃРёРјРІРѕР»Р°РјРё, СЏРІР»СЏСЋС‰РёРјРёСЃСЏ РїРµСЂРµРЅРѕСЃРѕРј, РїСЂРѕР±РµР»РѕРј РёР»Рё until-СЃРёРјРІРѕР»РѕРј */
-void StringReadLn(Vector* this, FILE* from); /* Р§РёС‚Р°РµС‚ СЃС‚СЂРѕРєСѓ РІРјРµСЃС‚Рµ СЃ РїСЂРѕР±РµР»Р°РјРё РІРїР»РѕС‚СЊ РґРѕ РїРµСЂРµРЅРѕСЃР° */
-void StringIgnore(FILE* from); /* РџРµСЂРµРЅРѕСЃРёС‚ РєСѓСЂСЃРѕСЂ С‡С‚РµРЅРёСЏ РЅР° СЃР»РµРґСѓСЋС‰СѓСЋ СЃС‚СЂРѕРєСѓ */
+void StringRead(Vector* this, char until, FILE* from); /* Читает строку между двумя символами, являющимися переносом, пробелом или until-символом */
+void StringReadLn(Vector* this, FILE* from); /* Читает строку вместе с пробелами вплоть до переноса */
+void StringIgnore(FILE* from); /* Переносит курсор чтения на следующую строку */
 
-bool StringToInt(char* from, long long* towhat, long long MinBound, long long MaxBound); /* РџС‹С‚Р°РµС‚СЃСЏ РїСЂРµРѕР±СЂР°Р·РѕРІР°С‚СЊ СЃС‚СЂРѕРєСѓ РІ С†РµР»РѕРµ С‡РёСЃР»Рѕ РІ РїСЂРµРґРµР»Р°С… [MinBound, MaxBound] */
-bool StringToFloat(char* from, double* towhat); /* РџС‹С‚Р°РµС‚СЃСЏ РїСЂРµРѕР±СЂР°Р·РѕРІР°С‚СЊ СЃС‚СЂРѕРєСѓ РІ РІРµС‰РµСЃС‚РІРµРЅРЅРѕРµ С‡РёСЃР»Рѕ */
+bool StringToInt(char* from, long long* towhat, long long MinBound, long long MaxBound); /* Пытается преобразовать строку в целое число в пределах [MinBound, MaxBound] */
+bool StringToFloat(char* from, double* towhat); /* Пытается преобразовать строку в вещественное число */
 
-long long StringReadInt(FILE* from, long long MinBound, long long MaxBound); /* Р—Р°РїСЂР°С€РёРІР°РµС‚ С†РµР»РѕРµ С‡РёСЃР»Рѕ РІ РїСЂРµРґРµР»Р°С… [MinBound, MaxBound] */
-double StringReadFloat(FILE* from); /* Р—Р°РїСЂР°С€РёРІР°РµС‚ РІРµС‰РµСЃС‚РІРµРЅРЅРѕРµ С‡РёСЃР»Рѕ */
-/* РЎС‚СЂРѕРєР° */
+long long StringReadInt(FILE* from, long long MinBound, long long MaxBound); /* Запрашивает целое число в пределах [MinBound, MaxBound] */
+double StringReadFloat(FILE* from); /* Запрашивает вещественное число */
+/* Строка */
 
-/* РњР°С‚РµРјР°С‚РёРєР° */
-double absf(double x); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ РјРѕРґСѓР»СЊ РІРµС‰РµСЃС‚РІРµРЅРЅРѕРіРѕ С‡РёСЃР»Р° */
-long long fact_int(int x); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ С†РµР»РѕС‡РёСЃР»РµРЅРЅС‹Р№ С„Р°РєС‚РѕСЂРёР°Р» С‡РёСЃР»Р° x */
-double fact_float(int x); /* Р’РѕР·РІСЂР°С‰Р°РµС‚ РІРµС‰РµСЃС‚РІРµРЅРЅС‹Р№ С„Р°РєС‚РѕСЂРёР°Р» С‡РёСЃР»Р° x */
-/* РњР°С‚РµРјР°С‚РёРєР° */
+/* Математика */
+double absf(double x); /* Возвращает модуль вещественного числа */
+long long fact_int(int x); /* Возвращает целочисленный факториал числа x */
+double fact_float(int x); /* Возвращает вещественный факториал числа x */
+/* Математика */
 
+/* Сравнение данных */
+bool OperLess(void* a, void* b, bool (*cmp)(void*, void*));
+bool OperEqual(void* a, void* b, bool (*cmp)(void*, void*));
+bool OperGreat(void* a, void* b, bool (*cmp)(void*, void*));
+
+bool CmpInt(void* a, void* b);
+bool CmpDouble(void* a, void* b);
+bool CmpChar(void* a, void* b);
+bool CmpCharArray(void* a, void* b);
+/* Сравнение данных */
+
+/* Set */
+typedef struct {
+    int elemsize;
+    int size;
+    Vector* base;
+    bool (*comp) (void* a, void* b);
+} Set;
+
+Set* SetNew(int sizeof_element, bool (*comparator) (void* a, void* b));
+void SetClear(Set* this) ;
+void SetDelete(Set* this);
+void SetAddElem(Set* this, void* what);
+void SetDelElem(Set* this, void* what);
+bool SetContainElem(Set* this, void* what);
+int SetSize(Set* this);
+/* Set */
+
+
+/* Map */
+typedef struct {
+    int keysize;
+    int valuesize;
+    int size;
+    Vector* keys;
+    Vector* values;
+    bool (*comp) (void* a, void* b);
+    void (*nuller) (void* a);
+} Map;
+
+Map* MapNew(int sizeof_key, int sizeof_value, bool (*comparator) (void* a, void* b), void (*value_nullator) (void* a));
+void MapClear(Map* this);
+void MapDelete(Map* this);
+bool MapContainElem(Map* this, void* what);
+void* MapValue(Map* this, void* what);
+int MapSize(Map* this);
+
+void NullInt(void* a);
+/* Map */
 
 #endif
